ff028fe46e3bcccc4d866d996f0c29f253aceb65
Layanne Pereira <layannepereiraa28@gmail.com>
2022-07-17 18:20:17 -0300

    Toast

--- frontend/src/components/NotificationButton/index.tsx
+++ frontend/src/components/NotificationButton/index.tsx
@@ -1,26 +1,27 @@
 import axios from 'axios';
+import { toast } from 'react-toastify';
 import icon from '../../assets/img/notification-icon.svg';
 import { BASE_URL } from '../../utils/request';
 
-import './styles.css';
+import'./styles.css';
 
 type Props = {
-  saleId: number;
+    saleId: number;
 }
 
-function handleClick(id: number) {
-  axios(`${BASE_URL}/sales/${id}/notification`)
+function handleClick(id : number){
+    axios(`${BASE_URL}/sales/${id}/notification`)
     .then(response => {
-      console.log("Sucesso")
+        toast.info("SMS enviado com sucesso");
     });
 }
 
-function NotificationButton({ saleId }: Props) {
-  return (
-    <div className="dsmeta-red-btn" onClick={() => { handleClick(saleId) }}>
-      <img src={icon} alt="Notificar" />
-    </div>
-  )
+function NotificationButton({saleId} : Props) {
+    return (
+        <div className="dsmeta-red-btn" onClick={()=> {handleClick(saleId)}}>
+            <img src={icon} alt="Notificar" />
+        </div>
+    )
 
 }
 
